6368d1fceb8010b53f9f1c5271d1a5b2ad1e003b
jeongwook <jeongwook.yu@utexas.edu>
2020-01-21 20:25:24 -0800

    c02 complete. Basic javascript instructions.

---
+++ c02/js/shorthand-variable.js
@@ -0,0 +1,15 @@
+var price = 5;
+var quantitiy = 14;
+var total = price * quantitiy;
+
+var price, quantitiy, total;
+price = 5;
+quantitiy = 14;
+total = price * quantitiy
+
+var price = 5, quantitiy = 14;
+var total = price * quantitiy;
+
+// Write total into the element with id of cost
+var el = document.getElementById('cost');
+el.textContent = '$' + total;
---
+++ c02/js/update-array.js
@@ -0,0 +1,11 @@
+// Create the array
+var colors = ['white', 'black', 'custom'];
+
+// Update the third item in the array
+colors[2] = 'beige';
+
+// Get the element with the id of colors
+var el = document.getElementById('colors');
+
+// Replace with third item from the array
+el.textContent = colors[2];
