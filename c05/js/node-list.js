e0e8961515e72a64087d97767d17467270f876e2
jeongwook <jeongwook.yu@utexas.edu>
2020-01-23 20:26:26 -0800

    c05 complete. The DOM

---
+++ c05/js/node-list.js
@@ -0,0 +1,9 @@
+var hotItems = document.querySelectorAll('li.hot'); // Store the NodeList in array
+
+if (hotItems.length > 0) {                          // If it contains items
+
+    for(var i = 0; i < hotItems.length; i++) {      // Loop through each item
+        hotItems[i].className = 'cool';             // Change the value of class attribute
+    }
+    
+}
